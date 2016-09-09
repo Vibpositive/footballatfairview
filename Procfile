dcdc359b6138133e3673fc99bc704c41cbfbf03f
Gabriel Oliveira <ras.vibpositive@gmail.com>
2016-09-09 18:39:54 +0100

    Demo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node ./footballatfairview/app.js
--- footballatfairview/app.js
+++ footballatfairview/app.js
@@ -79,6 +79,32 @@ app.set('permission', {
   notAuthorized: notAuthorized
 });
 
+
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
 app.use(morgan('dev'));
 
 app.use(cookieParser());
